938f6888ede0683f5bcebb3294c7567f48efda54
daimonhunter <daimoon.hunter@gmail.com>
2015-06-11 13:47:27 +0800

    blade demo and helpers function

---
+++ app/Extensions/Function/helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Container\Container;
+
+if (!function_exists('helpEx')) {
+    function helpEx()
+    {
+        echo 'helper';
+    }
+}
+
---
+++ database/migrations/2014_10_12_000000_create_wb_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWbUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('wb_users', function(Blueprint $table)
+		{
+			$table->increments('uid');
+            $table->string('nickname')->comment = '昵称';
+			$table->char('mobile',11)->unique()->comment = '手机号';
+			$table->string('password', 60)->comment = '密码';
+            $table->tinyInteger('type',FALSE,TRUE)->comment = '用户类型,1:普通用户;2:投资人;etc..';
+            $table->tinyInteger('status',FALSE,TRUE)->comment = '用户状态';
+			$table->rememberToken();
+            $table->unsignedInteger('create_at');   //创建时间
+            $table->unsignedInteger('update_at');   //更新时间
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
